d54dfd263aaf2cdf270a2e9fc7c2a46315251f36
zzwu <zzwu@zzwu.local>
2013-04-25 10:04:04 +0800

    zjh poker

---
+++ src/srg/zjh_poker.clj
@@ -0,0 +1,104 @@
+(ns srg.zjh-poker
+  (:refer-clojure :exclude [flush])
+  (:require [srg.cards :as cards]))
+
+(def patterns [:bomb :straight-flush :flush :straight :pair :high-card])
+
+(defn score
+  [[parent first second third]]
+  (apply + (map * [third second first parent]
+                (iterate #(* % 15) 1))))
+
+(defn sort-by-rank-reverse
+  [cards]
+  (into []
+        (reverse (sort-by :rank cards))))
+
+(defn bomb
+  "vector in, vector out."
+  [cards]
+  (if (apply = (map :rank cards))
+    cards))
+
+(defn flush
+  [cards]
+  (if (apply = (map :suit cards))
+    (sort-by-rank-reverse cards)))
+
+(defn serial?
+  [nums]
+  (let [l (drop-last nums)
+        r (map dec (rest nums))]
+    (every? true? (map = l r))))
+
+(defn straight
+  [cards]
+  (if (serial? (sort (map :rank cards)))
+    (sort-by-rank-reverse cards)))
+
+(defn straight-flush
+  [cards]
+  (if (and (straight cards) (flush cards))
+    (sort-by-rank-reverse cards)))
+
+(defn pair-num
+  [nums]
+  (for [l (range (count nums))
+        r (range (count nums))
+        :when (not= l r)]
+    (if (= (nums l) (nums r)) (nums l))))
+
+(defn pair
+  [cards]
+  (if-let [pair-rank (first (map identity
+                                 (pair-num (into [] (map :rank cards)))))]
+    (let [pair-cards (filter #(= pair-rank (:rank %)) cards)
+          left-card (first (filter #(not= pair-rank (:rank %)) cards))]
+      [(first pair-cards) (second pair-cards) left-card])))
+
+(defn high-card
+  [cards]
+  (sort-by-rank-reverse cards))
+
+(def patterns-matchfns (map #(vector %1 %2) patterns [bomb straight-flush flush straight pair high-card]))
+
+(defn index-of
+  [type patterns]
+  (first
+   (keep-indexed #(if (= type %2) %1) patterns)))
+
+(defn match-result
+  [type cards]
+  (let [pattern-index (index-of type patterns)
+        score-factor (into [(- (count patterns) pattern-index)] (map :rank cards))]
+    {:pattern type
+     :score (score score-factor)
+     :cards cards}))
+
+(defn match
+  [cards]
+  (filter identity
+       (map (fn [[pattern matchfn]]
+              (if-let [res (matchfn cards)]
+                (match-result pattern res))) patterns-matchfns)))
+
+(defn diff352?
+  "花色不同的352"
+  [cards]
+  (let [ranks-set (set (map :rank cards))
+        suit-set (set (map :suit cards))]
+    (and (= #{3 5 2} ranks-set)
+         (= 3 (count suit-set)))))
+
+(defn pk
+  [left-cards right-cards]
+  (cond
+   (and (diff352? left-cards) (bomb right-cards)) :win
+   (and (bomb left-cards) (diff352? right-cards)) :lose
+   :else (let [left-cards-score (:score (first (match left-cards)))
+               right-cards-score (:score (first (match right-cards)))]
+           (cond
+            (> left-cards-score right-cards-score) :win
+            (< left-cards-score right-cards-score) :lose
+            (= left-cards-score right-cards-score) :draw))))
+
---
+++ test/srg/zjh_poker_test.clj
@@ -0,0 +1,39 @@
+(ns srg.zjh-poker-test
+  (:refer-clojure :exclude [flush])
+  (:use [srg.zjh-poker]
+        [midje.sweet]))
+
+(facts "test bomb"
+       (bomb [{:rank 3, :suit :spades} {:rank 5, :suit :spades} {:rank 4, :suit :spades}]) => nil
+       (bomb [{:rank 3, :suit :spades} {:rank 3, :suit :hearts} {:rank 3, :suit :diamonds}]) => [{:rank 3, :suit :spades} {:rank 3, :suit :hearts} {:rank 3, :suit :diamonds}])
+
+(facts "test flush"
+       (flush [{:rank 3, :suit :spades} {:rank 3, :suit :hearts} {:rank 3, :suit :diamonds}]) => nil
+       (flush [{:rank 3, :suit :spades} {:rank 5, :suit :spades} {:rank 4, :suit :spades}]) => [{:rank 5, :suit :spades} {:rank 4, :suit :spades} {:rank 3, :suit :spades}])
+
+(facts "test straight"
+       (straight [{:rank 7, :suit :spades} {:rank 5, :suit :spades} {:rank 4, :suit :spades}]) => nil
+       (straight [{:rank 3, :suit :spades} {:rank 5, :suit :spades} {:rank 4, :suit :spades}]) => [{:rank 5, :suit :spades} {:rank 4, :suit :spades} {:rank 3, :suit :spades}])
+
+(facts "test straight-flush"
+       (straight-flush [{:rank 5, :suit :spades} {:rank 4, :suit :spades} {:rank 7, :suit :spades}]) => nil
+       (straight-flush [{:rank 5, :suit :spades} {:rank 4, :suit :spades} {:rank 3, :suit :spades}]) => [{:rank 5, :suit :spades} {:rank 4, :suit :spades} {:rank 3, :suit :spades}])
+
+(facts "test pair"
+       (pair [{:rank 5, :suit :spades} {:rank 4, :suit :spades} {:rank 3, :suit :spades}]) => nil
+       (pair [{:rank 5, :suit :spades} {:rank 5, :suit :hearts} {:rank 3, :suit :spades}]) => [{:rank 5, :suit :spades} {:rank 5, :suit :hearts} {:rank 3, :suit :spades}])
+
+(facts "test high-card"
+       (high-card [{:rank 5, :suit :spades} {:rank 3, :suit :hearts} {:rank 5, :suit :spades}]) => [{:rank 5, :suit :spades} {:rank 5, :suit :spades} {:rank 3, :suit :hearts}])
+
+(facts "test match"
+       (match [{:rank 5, :suit :spades} {:rank 5, :suit :spades} {:rank 3, :suit :hearts}]) =>  (list {:cards [{:rank 5, :suit :spades} {:rank 5, :suit :spades} {:rank 3, :suit :hearts}], :pattern :pair, :score 7953} {:cards [{:rank 5, :suit :spades} {:rank 5, :suit :spades} {:rank 3, :suit :hearts}], :pattern :high-card, :score 4578})
+       (match [{:rank 3, :suit :spades} {:rank 5, :suit :spades} {:rank 4, :suit :spades}]) => (list {:cards [{:rank 5, :suit :spades} {:rank 4, :suit :spades} {:rank 3, :suit :spades}], :pattern :straight-flush, :score 18063} {:cards [{:rank 5, :suit :spades} {:rank 4, :suit :spades} {:rank 3, :suit :spades}], :pattern :flush, :score 14688} {:cards [{:rank 5, :suit :spades} {:rank 4, :suit :spades} {:rank 3, :suit :spades}], :pattern :straight, :score 11313} {:cards [{:rank 5, :suit :spades} {:rank 4, :suit :spades} {:rank 3, :suit :spades}], :pattern :high-card, :score 4563}))
+
+
+(facts "test pk"
+       (pk [{:rank 5, :suit :spades} {:rank 2, :suit :diamonds} {:rank 3, :suit :hearts}] [{:rank 5, :suit :spades} {:rank 5, :suit :diamonds} {:rank 5, :suit :hearts}]) => :win
+       (pk [{:rank 5, :suit :spades} {:rank 5, :suit :diamonds} {:rank 5, :suit :hearts}] [{:rank 5, :suit :spades} {:rank 2, :suit :diamonds} {:rank 3, :suit :hearts}]) => :lose
+       (pk [{:rank 5, :suit :spades} {:rank 4, :suit :diamonds} {:rank 3, :suit :hearts}] [{:rank 5, :suit :spades} {:rank 5, :suit :diamonds} {:rank 5, :suit :hearts}]) => :lose
+       (pk [{:rank 5, :suit :spades} {:rank 4, :suit :spades} {:rank 3, :suit :spades}] [{:rank 5, :suit :spades} {:rank 5, :suit :diamonds} {:rank 5, :suit :hearts}]) => :lose
+       (pk [{:rank 5, :suit :spades} {:rank 4, :suit :diamonds} {:rank 3, :suit :hearts}] [{:rank 5, :suit :diamonds} {:rank 3, :suit :diamonds} {:rank 4, :suit :hearts}]) => :draw)
