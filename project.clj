adcfafe4bfe9db074f192c53b7a4cc7bc572fd6e
zzwu <greatzzwu@gmail.com>
2013-11-08 10:22:09 +0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [aleph "0.3.0-beta7"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [log4j "1.2.16"]]
+                 [log4j "1.2.16"]
+                 [midje "1.5.1"]]
   :plugins [[lein-midje "3.1.1"]])
